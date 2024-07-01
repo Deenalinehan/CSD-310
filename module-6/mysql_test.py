855e233d1e1be5e932d84e51855744bcb4f243f0
Deena Linehan <deenalinehan@gmail.com>
2024-06-30 19:54:40 -0500

    db_init_2022

---
+++ module-6/mysql_test.py
@@ -0,0 +1,13 @@
+#Deena Linehan
+#Module 6
+
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user": "movies_user",
+    "password": "popcorn",
+    "host": "127.0.0.1",
+    "database": "movies",
+    "raise_on_warnings": True
+}
